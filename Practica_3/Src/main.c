a57f1d2c015ca1edbc1cea7661076b21cc63d8cf
fm <fm@www2noc.bbt.net.ar>
2023-07-06 23:03:50 -0300

    Practica 3 version 1.1 (Estructura de carpetas segun PdM)

--- Practica_3/Src/main.c
+++ Practica_3/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+
 #include "API_delay.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
